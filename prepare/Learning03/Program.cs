6f263e128d2100ae110fccea60a1b32719c62217
Trevaye <queenieattitude@byui.edu>
2024-05-09 22:26:42 -0500

    Finished Learning 03

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,29 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Creating a Fraction object with default constructor (1/1)
-        Fraction f1 = new Fraction();
-        // Printing the fraction
-        Console.WriteLine(f1.GetFractionString());
-        // Printing the decimal
-        Console.WriteLine(f1.GetDecimalValue());
-
-        // Creating a Fraction object with default constructor (5/1)
-        Fraction f2 = new Fraction(5);
-        //Printing the fraction and the decimal
-        Console.WriteLine(f2.GetFractionString());
-        Console.WriteLine(f2.GetDecimalValue());
-
-        // Creating a Fraction object with constructor for fraction (3/4)
-        Fraction f3 = new Fraction(3, 4);
-        // Printing the fraction and the decimal
-        Console.WriteLine(f3.GetFractionString());
-        Console.WriteLine(f3.GetDecimalValue());
-
-        // Creating a Fraction object with constructor for fraction (1/3)
-        Fraction f4 = new Fraction(1, 3);
-        // Printing the fraction and the decimal
-        Console.WriteLine(f4.GetFractionString());
-        Console.WriteLine(f4.GetDecimalValue());
+        
     }
 }
